092eb646179dd56544d8fb14146432c22602721c
Yahvannel <yahvannellopez@gmail.com>
2024-07-14 18:06:16 -0500

    completed code for login auth and format date in utils folder

---
+++ utils/auth.js
@@ -0,0 +1,10 @@
+// redirect user request to login route if not logged in 
+const auth = (req, res, next) => {
+    if (!req.session.logged_in) {
+        res.redirect('/login');
+    } else {
+        next();
+    }
+};
+
+module.exports = auth;
---
+++ utils/helpers.js
@@ -0,0 +1,6 @@
+// format date mm/dd/yyy
+module.exports = {
+    format_date: (date) => {
+        return date.toLocaleDateString();
+    },
+};
